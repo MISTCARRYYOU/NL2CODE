d8ac646c1f6543eb778d2ac9aaf2f584ae15d5cb
Nathanael BEAU <n.beau@LGOP3542.local>
2020-04-15 15:04:05 +0200

    second commit

--- dataset/preprocess_dataset.py
+++ dataset/preprocess_dataset.py
@@ -1,5 +1,43 @@
 import json
+import sys
+import traceback
 
-with open('./data_conala/conala-test.json') as json_data:
-    data_dict = json.load(json_data)
-    print(data_dict)
+import nltk
+# nltk.download('punkt')
+
+from dataset.canonicalize import *
+from dataset.util import get_encoded_code_tokens
+
+if __name__ == '__main__':
+    file_path = './data_conala/'
+
+    for file_path in [(file_path + 'conala-train.json'), (file_path + 'conala-test.json')]:
+        print('file {}'.format(file_path), file=sys.stderr)
+
+        dataset = json.load(open(file_path))
+
+        for i, example in enumerate(dataset):
+            intent = example['intent']
+            rewritten_intent = example['rewritten_intent']
+            snippet = example['snippet']
+            intent_tokens = []
+
+            if rewritten_intent:
+                try:
+                    canonical_intent, slot_map = canonicalize_intent(rewritten_intent)
+                    encoded_reconstr_code = get_encoded_code_tokens(snippet)
+                except:
+                    print('*' * 20, file=sys.stderr)
+                    print(i, file=sys.stderr)
+                    print(intent, file=sys.stderr)
+                    print(snippet, file=sys.stderr)
+                    traceback.print_exc()
+
+                    failed = True
+            if not intent_tokens:
+                intent_tokens = nltk.word_tokenize(intent)
+
+            example['intent_tokens'] = intent_tokens
+            example['snippet_tokens'] = snippet
+
+        json.dump(dataset, open(file_path + '.seq2seq', 'w'), indent=2)
---
+++ dataset/util.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+
+import token as tk
+from tokenize import generate_tokens
+
+from io import StringIO
+
+
+def encode_tokenized_code(code_tokens):
+    tokens = []
+    for token in code_tokens:
+        if token == '\t':
+            tokens.append('_TAB_')
+        elif token == '\n':
+            tokens.append('_NEWLINE_')
+
+
+def get_encoded_code_tokens(code):
+    """
+    Transform code string into a tokenized form
+    :param code: snippet of code from CoNaLa
+    :return: list of tokenized code
+    """
+    code = code.strip() #remove space at the beginning/end
+    token_stream = generate_tokens(StringIO(code).readline) #tokenize code between OP/NAME/NEWLINE/INDENT
+    tokens = []
+    indent_level = 0
+    new_line = False
+
+    for toknum, tokval, _, _, _ in token_stream:
+        if toknum == tk.NEWLINE:
+            tokens.append('#NEWLINE#')
+            new_line = True
+        elif toknum == tk.INDENT:
+            indent_level += 1
+        elif toknum == tk.STRING:
+            tokens.append(tokval.replace(' ', '#SPACE#').replace('\t', '#TAB#').replace('\r\n', '#NEWLINE#').replace('\n', '#NEWLINE#'))
+        elif toknum == tk.DEDENT:
+            indent_level -= 1
+        else:
+            tokval = tokval.replace('\n', '#NEWLINE#')
+            if new_line:
+                for i in range(indent_level):
+                    tokens.append('#INDENT#')
+
+            new_line = False
+            tokens.append(tokval)
+
+    # remove ending None
+    if len(tokens[-1]) == 0:
+        tokens = tokens[:-1]
+
+    if '\n' in tokval:
+        pass
+
+    return tokens
+
+
+
+
