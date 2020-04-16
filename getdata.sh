c85b2bad7d9520cb254c95e90bde6f86cced2a4e
Nathanael BEAU <n.beau@LGOP3542.local>
2020-04-16 16:37:03 +0200

    change .sh file

--- getdata.sh
+++ getdata.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/Jonor127-OP/NL2CODE.git
 
-cd Desktop/NL2CODE/dataset/data_conala
-
+cd ./dataset/data_conala
 
 # Get the data
 wget http://www.phontron.com/download/conala-corpus-v1.1.zip
