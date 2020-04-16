c138abf6df56ced2714a47ffc69c78af2ce2b47d
Nathanael BEAU <n.beau@LGOP3542.local>
2020-04-16 18:14:40 +0200

    maj .sh

--- getdata.sh
+++ getdata.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
+# Create environment
+conda create --name myenv --file spec-file.txt
 
+
+# Go to folder for data
 cd ./dataset/data_conala
 
 # Get the data
 wget http://www.phontron.com/download/conala-corpus-v1.1.zip
 unzip conala-corpus-v1.1.zip
-
 rm -r conala-corpus-v1.1.zip
