3e2017520753ba5678cdfa3101235fa9e42ae2db
Nathanael BEAU <n.beau@LGOP3542.local>
2020-04-17 18:00:52 +0200

    update README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # NL2CODE
+> Seq2seq model to traduce natural language into Python code.
+
+### Installation
+
+```bash
+git clone https://github.com/Jonor127-OP/NL2CODE.git
+cd NL2CODE
+bash ./getdata.sh  # get datasets
+```
+
+
+**Steps**
+
+- First: run the seq2seq model without attention on CoNaLa.
+- Second: run seq2seq model with attention on CoNaLa.
+- Third: add a bi-LSTM in the encoder and run it.
+- Fourth: Configure decoder to predict action rather than code.
+
--- model/seq2seq.py
+++ model/seq2seq.py
@@ -8,44 +8,6 @@ import torch.nn.functional as F
 import nltk
 
 
-class Lang:
-    def __init__(self, name):
-        self.name = name
-        self.word2index = {}
-        self.word2count = {}
-        self.index2word = {0: "SOS", 1: "EOS"}
-        self.n_words = 2  # Count SOS and EOS
-
-    def add_sentence(self, sentence):
-        for word in sentence.split(' '):
-            self.addWord(word)
-
-    def add_word(self, word):
-        if word not in self.word2index:
-            self.word2index[word] = self.n_words
-            self.word2count[word] = 1
-            self.index2word[self.n_words] = word
-            self.n_words += 1
-        else:
-            self.word2count[word] += 1
-
-    def prepareData(self, lang1, lang2):
-
-        input_lang, output_lang, pairs = self.readLangs(lang1, lang2)
-        print("Read %s sentence pairs" % len(pairs))
-        print("Trimmed to %s sentence pairs" % len(pairs))
-
-        for pair in pairs:
-            input_lang.addSentence(pair[0])
-            output_lang.addSentence(pair[1])
-
-        print("Counted words:")
-        print(input_lang.name, input_lang.n_words)
-        print(output_lang.name, output_lang.n_words)
-
-        return input_lang, output_lang, pairs
-
-
 class EncoderRNN(nn.Module):
 
     def __init__(self, input_size, hidden_size):
