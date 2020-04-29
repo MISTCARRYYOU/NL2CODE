b05daafb132d91ede031e3b9feb4e9236ca76dde
Nathanael BEAU <n.beau@LGOP3542.local>
2020-04-29 16:38:36 +0200

    change name config

--- config/utils.py
+++ config/utils.py
@@ -4,6 +4,7 @@ import argparse
 def init_arg_parser():
     arg_parser = argparse.ArgumentParser()
 
+    # PATHS
     arg_parser.add_argument('--raw_path', default='./dataset/data_conala/', type=str,
                             help='path to the raw target file')
     arg_parser.add_argument('--train_path', default=10, type=str,
@@ -11,7 +12,8 @@ def init_arg_parser():
     arg_parser.add_argument('--test_path', default=10, type=str,
                             help='path to the eval model file')
 
-    arg_parser.add_argument('--mode', type=str,
-                            help='path to the raw target file')
+    # MODE
+    arg_parser.add_argument('--mode', choices=['train', 'test'], required=True,
+                            help='Run mode')
 
     return arg_parser
